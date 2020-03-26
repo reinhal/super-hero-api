0e0d9830745c68c07700d724c0c3cd729d91ebd0
John Carmichael <jlcarmic@gmail.com>
2020-03-26 11:31:05 -0400

    Finished state at the end of the Leveling Up section

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
-const { getAllHeroes, getHeroBySlug } = require('./controllers/heroes')
+const bodyParser = require('body-parser')
+const { getAllHeroes, getHeroBySlug, saveNewHero } = require('./controllers/heroes')
 
 const app = express()
 
 app.get('/', getAllHeroes)
 
 app.get('/:slug', getHeroBySlug)
+
+app.post('/', bodyParser.json(), saveNewHero)
 
 app.listen(1337, () => {
   console.log('Listening on port 1337...') // eslint-disable-line no-console
